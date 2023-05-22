44f3896ef92d96f15c14b12ecc0d144d8e74cf81
Alexxoder <47437939+Alexxoder@users.noreply.github.com>
2023-05-22 13:31:20 -0300

    Add files via upload

---
+++ Practice/python/canConstruct.py
@@ -0,0 +1,38 @@
+
+#Write a function `canConstruct(target, WordBank)` that accepts a target string and an array of strings.
+#The function should return a boolean 
+#indicating whether or not the `target` can be constructed by concatenating elements of the `wordBank` array.
+#You may reuse elements of `wordBank` as many times as needed.
+
+def beginsWith(list,word):
+    if(len(word) > len(list)):
+        return False
+    rem = list[:len(word)]
+    return word == rem
+
+def canConstruct(target, tuple, memo):
+    if(target in memo):
+        return memo[target]
+    if (len(target) == 0):
+        return True
+    
+    for w in tuple:
+        if (beginsWith(target,w)):
+            remainder = target[len(w):]
+            if(canConstruct(remainder,tuple,memo) == True):
+                memo[remainder] = True
+                return True
+    memo[target] = False
+    return False
+
+words1 = ("ab","abc","cd","def","abcd")
+target1 = "abcdef"
+target2 = "skateboard"
+words2 = ("bo","rd","ate","t","ska","sk","boar")
+a = ""
+memo = {}
+memo1 = {}
+memo2 = {}
+print(canConstruct(a,words1,memo))
+print(canConstruct(target1,words1,memo1))
+print(canConstruct(target2,words2,memo2))
